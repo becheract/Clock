b631311c8fa4e84d7148883c0865bc3be44bb056
Bechera <becherachapman@gmail.com>
2021-05-11 23:28:19 -0400

    circle colour

--- script.js
+++ script.js
@@ -30,14 +30,35 @@ document.getElementById("Horloge").innerHTML = currentTimeString;
 //function will animate background
 function animate() {
 const canvas = document.getElementById('circle');
-
+//create a circle 
 if(canvas.getContext){
+    //2d 
     let ctx = canvas.getContext('2d');
     let x = canvas.width / 2;
     let y = canvas.height / 2;
     let r = 45; 
 
     ctx.beginPath();
-}
-}
+    ctx.arc(x,y,r,0,2 *Math.PI, false);
+    ctx.linewidth = 3;
+    ctx.fillStyle = 'red';
+    ctx.fill();
 
+    //will create 20 circles randomly in the canvas
+    for (let x = 0; x < 20; x++) {
+        //get random x and y coordinates 
+        let ex = Math.floor(Math.random() * 10);
+        let why = Math.floor(Math.random() * 10);
+
+        console.log(`${ex}   ${why}`);
+        
+        ctx.beginPath();
+        ctx.arc(ex,why,r,0,2 *Math.PI, false);
+        ctx.linewidth = 3;
+        //random colour picker
+        let randomColour = "#"+Math.floor(Math.random()*16777215).toString(16);
+        ctx.fillStyle = randomColour;
+        ctx.fill();
+    }
+}
+}
